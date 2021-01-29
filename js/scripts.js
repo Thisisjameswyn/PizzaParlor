615914a94007a759a11d2f8959ee651dfd426c0b
James Wyn <thisisjameswyn@gmail.com>
2021-01-29 10:41:29 -0800

    add html selectors and plug in calc logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ function Pizza(size, topping1, topping2, topping3)  {
   this.topping1 = topping1;
   this.topping2 = topping2;
   this.topping3 = topping3;
+  this.toppingAmount = topping1 + topping2 + topping3;
   this.price = 0;
 }
 
 Pizza.prototype.calcCharge = function() {
-  //calc price for size
   if(this.size == 2)  {
     this.price += 8;
   } else if (this.size == 1)  {
@@ -15,16 +15,28 @@ Pizza.prototype.calcCharge = function() {
   } else {
     this. price += 4;
   }
+
+  if(this.toppingAmount > 2) {
+    this.price += 6;
+  } else if (this.toppingAmount < 2) {
+    this.price += 2;
+  } else {
+    this.price += 4;
+  }
+
   //calc price for topping nunmber
 
   //calc price for special toppings
 }
 
 $(document).ready(function()  {
-  let pizzaPie = new Pizza(1,0,0,0);
-  pizzaPie.calcCharge();
-  console.log(pizzaPie);
-  // $("").submit(function(event) {
-
-  // });
+  $("form#pizza-submit").submit(function(event) {
+    event.preventDefault();
+    let pizzaPie = new Pizza(parseInt($("#size").val()),parseInt($("#topping1").val()),parseInt($("#topping2").val()),parseInt($("#topping3").val()));
+    console.log(pizzaPie);
+    pizzaPie.calcCharge();
+    console.log(pizzaPie.price);
+    
+    // let pizzaTime = $("#movie option:selected").text();
+  });
 });
